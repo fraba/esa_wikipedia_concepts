d134e8fdfb4ae060135c7317f67a234d9adeef4d
Francesco Bailo <me@francescobailo.net>
2016-04-27 06:36:19 +0200

    Add figure names

---
+++ auto/bibliography.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "gabrilovich_computing_2007"
+    "jarosciak_how_2013"
+    "manning_introduction_2008"
+    "van_der_maaten_visualizing_2008"
+    "ggplot2"
+    "dplyr"
+    "R"
+    "tsne"
+    "DBI"
+    "RMySQL"
+    "datatable"
+    "plyr"
+    "igraph"
+    "stringr"
+    "tm"
+    "SnowballC"
+    "slam"
+    "RColorBrewer"
+    "gridExtra")))
+
